c8af7eb9a36d764bc8b2911f87349fd1657bad47
paul.paik <ppaik4311@gmail.com>
2022-02-06 20:08:43 -0600

    Changed from html table to datatable for testing.

--- visualize_results.py
+++ visualize_results.py
@@ -4,32 +4,20 @@
 # 02/06/2022
 # Code for visualizing data on a dashboard format.
 
-from dash import Dash, html
+from dash import Dash, dash_table
 import pandas as pd
 from record_parser import yt_records_df as yt_data
 
-def generate_table(input_df, max_rows=10):
-    """
-    Generate a summary table.
-    """
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in input_df.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(input_df.iloc[i][col]) for col in input_df.columns
-            ]) for i in range(min(len(input_df), max_rows))
-        ])
-    ])
-
-
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.H4(children='My Youtube view history summary'),
-    generate_table(yt_data)
-])
+# Render a summary table showing list of viewed channels and view counts.
+app.layout = dash_table.DataTable(
+    data=yt_data.to_dict('records'),
+    columns=[{'id': column, 'name': column} for column in yt_data.columns],
+    style_cell={'textAlign': 'center'},
+    style_data={'border':'1px solid black'},
+    style_header={'border':'1px solid black'},
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
